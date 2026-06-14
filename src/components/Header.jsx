21cc6541c0d0f28924e475da734ee8a07fa64130
Tuan Ha <hatuan@email.cz>
2026-06-14 16:25:29 +0200

    Page height, Header styling + media queries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import pokemonLogoURL from "./../assets/pokemon-logo.png";
+import styles from "./../styles/Header.module.css";
+import pokemonLogoURL from "./../assets/images/pokemon-logo.png";
 import Navigation from "./Navigation";
 
 function Header() {
 
   return (
-    <header>
-      <div className="header-img-container">
+    <header className={styles["body-header"]}>
+      <div className={styles["header-image-container"]}>
         <img
           src={pokemonLogoURL}
           alt="Logo of Pokemon"
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,20 @@
-import { Link } from "react-router"
+import styles from "./../styles/Header.module.css";
+import { Link } from "react-router";
 
 function Navigation() {
   return (
-    <nav>
-      <Link>Homepage</Link>
-      <Link>Shop</Link>
-      <Link>About Us</Link>
-      <Link>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+    <nav className={styles["header-navigation"]}>
+      <Link className={`link ${styles["link-header-navigation"]}`}>Homepage</Link>
+      <Link className={`link ${styles["link-header-navigation"]}`}>Shop</Link>
+      <Link className={`link ${styles["link-header-navigation"]}`}>About Us</Link>
+      <Link className={`link ${styles["link-header-navigation"]}`}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" viewBox="0 0 24 24">
           <path d="M0 0h24v24H0z" fill="none" />
           <path fill="currentColor" d="m12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3" />
         </svg>
       </Link>
-      <Link>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+      <Link className={`link ${styles["link-header-navigation"]}`}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" viewBox="0 0 24 24">
 	        <path d="M0 0h24v24H0z" fill="none" />
 	        <path fill="currentColor" d="M5.588 21.413Q5 20.825 5 20t.588-1.412T7 18t1.413.588T9 20t-.587 1.413T7 22t-1.412-.587m10 0Q15 20.825 15 20t.588-1.412T17 18t1.413.588T19 20t-.587 1.413T17 22t-1.412-.587M6.15 6l2.4 5h7l2.75-5zM5.2 4h14.75q.575 0 .875.513t.025 1.037l-3.55 6.4q-.275.5-.737.775T15.55 13H8.1L7 15h12v2H7q-1.125 0-1.7-.987t-.05-1.963L6.6 11.6L3 4H1V2h3.25zm3.35 7h7z" />
         </svg>
