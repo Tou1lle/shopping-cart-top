b97c269d5c2e3ab3ab019730d07e50ef5880d208
Tuan Ha <hatuan@email.cz>
2026-06-14 17:43:38 +0200

    Create Footer component structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,58 @@
+import styles from "./../styles/Footer.module.css";
+import pokemonLogoURL from "./../assets/images/pokemon-logo.png";
+import { Link } from "react-router";
+
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles["footer-image-container"]}>
+        <img src={pokemonLogoURL} alt="Pokémon logo" />
+      </div>
+      <div>
+        <div>
+          <div>
+            <h3>Stay in touch!</h3>
+            <p>Stay up to date with the latest Pokémon TCG releases
+              , rare card drops, restocks, and exclusive offers.
+              Join our community of collectors and trainers and never miss the next big pull.
+            </p>
+          </div>
+          <form action="#" onSubmit={(e) => e.preventDefault()}>
+            <input type="text" />
+            <button>SUBMIT</button>
+          </form>
+        </div>
+        <div>
+          <div>
+            <h3>Shop</h3>
+            <nav>
+              <Link className={`link`}>Starter Pack</Link>
+              <Link className={`link`}>Rare Sets</Link>
+              <Link className={`link`}>I Choose You!</Link>
+            </nav>
+          </div>
+          <div>
+            <h3>Customer Service</h3>
+            <nav>
+              <Link className={`link`}>Shipping Information</Link>
+              <Link className={`link`}>Return & Refunds</Link>
+              <Link className={`link`}>FAQ</Link>
+              <Link className={`link`}>Contact</Link>
+            </nav>
+          </div>
+          <div>
+            <h3>About</h3>
+            <nav>
+              <Link className={`link`}>Our Story</Link>
+              <Link className={`link`}>Blog</Link>
+              <Link className={`link`}>Our Favorite Decks</Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,13 @@
 import "./../styles/main.css"
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <div className="main" style={{flexGrow: 1}}>Content</div>
+      <Footer />
     </>
   )
 }
