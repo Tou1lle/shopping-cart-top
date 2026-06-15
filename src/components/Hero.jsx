05b968f0d45e3c424d9e5f75dcf72a4242a41f9b
Tuan Ha <hatuan@email.cz>
2026-06-15 23:25:59 +0200

    Add Animated Gif pokemons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,21 @@
 import styles from "./../styles/Hero.module.css";
+import pokemonGifURL_1 from "./../assets/images/blastoise-animated.gif";
+import pokemonGifUrl_2 from "./../assets/images/drat-animated.gif"
 
 function Hero() {
   return (
     <div className={styles["hero-container"]}>
       <h1 className={styles["hero-header"]}>Most Trending Cards Right Now!</h1>
       <div className={styles["hero-content"]}>
-
+        <div className={styles["pokemon-animal-container-right"]}>
+          <img src={pokemonGifURL_1} className={styles["pokemon-animal-right"]} alt="Gif image of a Pokémon" />
+        </div>
+        <div className={styles["pokemon-animal-container-left"]}>
+          <img src={pokemonGifUrl_2} className={styles["pokemon-animal-left"]} alt="Gif image of a Pokémon" />
+        </div>
+        <div>
+          Top Selling Cards Items
+        </div>
       </div>
     </div>
   )
