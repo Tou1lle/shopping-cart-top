7ddbc5de4f926e9e6727c477c35262b8ffaf2eab
Tuan Ha <hatuan@email.cz>
2026-06-18 20:27:21 +0200

    Create outro component

---
+++ src/components/homepage/OutroImage.jsx
@@ -0,0 +1,11 @@
+import styles from "./../../styles/OutroImage.module.css";
+
+function OutroImage() {
+  return (
+    <div>
+      <img src="" alt="" />
+    </div>
+  )
+}
+
+export default OutroImage;
