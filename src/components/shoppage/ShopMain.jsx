e4e19dd728b2e3204a5183ebb11cde44432966bf
Tuan Ha <hatuan@email.cz>
2026-06-21 19:21:08 +0200

    Try out navigation with updated dynamic segment

--- src/components/shoppage/ShopMain.jsx
+++ src/components/shoppage/ShopMain.jsx
@@ -1,10 +1,13 @@
+import { useParams } from "react-router";
 import ShopFilters from "./ShopFilters";
 import ShopItems from "./ShopItems";
 
 function ShopMain() {
+  const { pageNumber } = useParams();
+
   return (
     <div>
-      <p style={{color: "var(--color-text-special)"}}>This is ShopMain</p>
+      <p style={{color: "var(--color-text-special)"}}>This is ShopMain page number: {pageNumber}</p>
       <ShopFilters />
       <ShopItems />
     </div>
--- src/components/shoppage/ShopPagination.jsx
+++ src/components/shoppage/ShopPagination.jsx
@@ -1,3 +1,20 @@
-function ShopPagination() {}
+import { useNavigate, useParams } from "react-router";
+
+function ShopPagination() {
+  const { pageNumber } = useParams();
+  const currentPage = Number(pageNumber);
+  const navigate = useNavigate();
+
+  const handleClick = (newPage) => {
+    navigate("/shop/" + newPage)
+  }
+
+  return (
+    <div>
+      <button>Previous</button>
+      <button onClick={() => handleClick(currentPage + 1)}>Next</button>
+    </div>
+  )
+}
 
 export default ShopPagination;
