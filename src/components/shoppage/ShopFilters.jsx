44e2d98876b5d9635e165166a0ebc5bc07466b55
Tuan Ha <hatuan@email.cz>
2026-06-22 14:18:49 +0200

    UseState to keep track of the active filter - clicked
    I initially used simple querySelector and manually
    added active class.
    
    I then switched to state and add active class base
    on which state is currently active
    - I have 4 filter buttons each with unique type
    - if state value = clicked type then active
    - this will also be more helpful for dropdowns
      to check which filter button is active

--- src/components/shoppage/ShopFilters.jsx
+++ src/components/shoppage/ShopFilters.jsx
@@ -1,16 +1,24 @@
 import styles from "./../../styles/shoppage/ShopFilters.module.css";
 import FilterSummary from "./FilterSummary";
 import FilterButton from "./FilterButton";
+import { FILTER_TYPES } from "../../utils/constants";
+import { useState } from "react";
 
 function ShopFilters({ totalCount }) {
+  const [active, setActive] = useState(null);
+
   return (
     <aside className={styles["sidebar"]}>
       <FilterSummary totalCount={totalCount} />
       <div className={styles["sidebar-filters"]}>
-        <FilterButton type="types" />
-        <FilterButton type="supertypes" />
-        <FilterButton type="subtypes" />
-        <FilterButton type="rarities" />
+        {FILTER_TYPES.map((type) => (
+          <FilterButton
+            key={type}
+            type={type}
+            active={active === type}
+            onClick={() => setActive(active === type ? null : type)}
+          />
+        ))}
       </div>
     </aside>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 const BASE_API_CARDS = "https://api.pokemontcg.io/v2/cards";
 const BASE_API = "https://api.pokemontcg.io/v2";
 const API_KEY = "41c725f5-be99-4d5c-9241-818f3a3f7322";
+const FILTER_TYPES = ["types", "supertypes", "subtypes", "rarities"];
 
-export { BASE_API_CARDS, BASE_API, API_KEY };
+export { BASE_API_CARDS, BASE_API, API_KEY, FILTER_TYPES };
