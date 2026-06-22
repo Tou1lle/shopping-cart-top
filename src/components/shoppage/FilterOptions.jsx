57ca27131770324b7426b143b07d151bde75ff57
Tuan Ha <hatuan@email.cz>
2026-06-22 22:16:59 +0200

    Include base option in data array + style dropdown
    The base option was created as separate button
    but that introduced a lot of duplication.
    This way we unshift it in the array at the baseOption
    is rendered with the rest.

--- src/components/shoppage/FilterOptions.jsx
+++ src/components/shoppage/FilterOptions.jsx
@@ -16,11 +16,35 @@ function FilterOptions({
       ) : error ? (
         <div>A network error has occured.</div>
       ) : (
-        <div>
-          <button onClick={() => setOption(baseOption)}>{baseOption}</button>
-          {data.map((option) => (
-            <button onClick={() => setOption(option)}>{option}</button>
-          ))}
+        <div className={styles["options-container"]}>
+          <div className={styles.options}>
+            {[baseOption, ...data].map((option) => (
+              <button
+                key={option}
+                className={`
+                  ${styles.option}
+                  ${currentOption === option ? styles["active-drop"] : ""}  
+                `}
+                onClick={() => setOption(option)}
+              >
+                {option}
+                {currentOption === option && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M0 0h16v16H0z" fill="none" />
+                    <path
+                      fill="currentColor"
+                      d="M13.864 3.655a.5.5 0 0 1-.021.707l-7.93 7.474a.6.6 0 0 1-.839-.016L2.394 9.1a.5.5 0 0 1 .712-.702l2.406 2.442l7.645-7.206a.5.5 0 0 1 .707.021"
+                    />
+                  </svg>
+                )}
+              </button>
+            ))}
+          </div>
         </div>
       )}
     </div>
