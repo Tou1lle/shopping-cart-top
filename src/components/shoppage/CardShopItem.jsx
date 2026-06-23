587ec8a9ca4cf3f9cf46d1135deed0f7cf221a5a
Tuan Ha <hatuan@email.cz>
2026-06-24 00:52:56 +0200

    Style card items

--- src/components/shoppage/CardShopItem.jsx
+++ src/components/shoppage/CardShopItem.jsx
@@ -1,22 +1,26 @@
 import { getCardPrice } from "../../utils/dataFetcher";
+import { Link } from "react-router";
+import styles from "./../../styles/shoppage/ShopItems.module.css";
 
 function CardShopItem({ card }) {
   return (
-    <div>
-      <div>
-        <img src={card.images.small} alt="" />
-      </div>
-      <div>
-        <div>
-          <h2>{card.name}</h2>
-          <p>{card.supertype}</p>
+    <Link className={`link ${styles["card-link"]}`}>
+      <div className={styles["card-container"]}>
+        <div className={styles["card-img-container"]}>
+          <img src={card.images.small} alt="Pokémon card" />
         </div>
-        <div>
-          <p>{card.rarity}</p>
-          <p>{getCardPrice(card)}</p>
+        <div className={styles["card-info-container"]}>
+          <div className={styles["card-main-info"]}>
+            <h2 className={styles["card-name"]}>{card.name}</h2>
+            <p className={styles["card-supertype"]}>{card.supertype}</p>
+          </div>
+          <div className={styles["card-secondary-info"]}>
+            <p className={styles["card-rarity"]}>{card.rarity}</p>
+            <p className={styles["card-price"]}>{getCardPrice(card)}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
