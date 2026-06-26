545fdd8f0e560f17ff2c0e58ec171d19b44100d5
Tuan Ha <hatuan@email.cz>
2026-06-27 01:49:35 +0200

    Add links to external shops

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,5 +1,6 @@
 import { useLocation, useParams, Link } from "react-router";
 import { useItemPokemonData } from "../utils/dataFetcher";
+import { MARKET_TYPES, MARKET_URLS } from "../utils/constants";
 import styles from "./../styles/itempage/ItemPage.module.css";
 
 function ItemPage() {
@@ -55,7 +56,7 @@ function ItemPage() {
             <img src={data.images.large} alt="" />
           </div>
           <div className={styles["left-side-content"]}>
-            <div>
+            <div className={styles["left-details-container"]}>
               <h2 className={styles["left-header"]}>
                 <span>{data.name}</span> <span>{data.id}</span>
               </h2>
@@ -69,7 +70,35 @@ function ItemPage() {
                 <span>Artist</span> <span>{data.artist}</span>
               </div>
             </div>
-            <div></div>
+            <div className={styles["left-links"]}>
+              {MARKET_TYPES.map((marketType) => {
+                return (
+                  <a
+                    href={
+                      data[marketType]
+                        ? data[marketType].url
+                        : MARKET_URLS[marketType]
+                    }
+                    target="_blank"
+                    className={styles["external-market-btn"]}
+                  >
+                    {marketType.charAt(0).toUpperCase() + marketType.slice(1)}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M0 0h24v24H0z" fill="none" />
+                      <path
+                        fill="currentColor"
+                        d="M14 3v2h3.59l-9.83 9.83l1.41 1.41L19 6.41V10h2V3m-2 16H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2z"
+                      />
+                    </svg>
+                  </a>
+                );
+              })}
+            </div>
           </div>
         </div>
         <div></div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,10 @@ const API_KEY = "41c725f5-be99-4d5c-9241-818f3a3f7322";
 const FILTER_TYPES = ["types", "supertypes", "subtypes", "rarities"];
 const PAGE_SIZE = 25;
 const MARKET_TYPES = ["tcgplayer", "cardmarket"];
+const MARKET_URLS = {
+  [MARKET_TYPES[0]]: "https://www.tcgplayer.com/",
+  [MARKET_TYPES[1]]: "https://www.cardmarket.com/en",
+};
 
 export {
   BASE_API_CARDS,
@@ -12,4 +16,5 @@ export {
   FILTER_TYPES,
   PAGE_SIZE,
   MARKET_TYPES,
+  MARKET_URLS,
 };
